499a53104269ec7867f1e7d4920cb57522ec347b
schriswin <s.chriswin2003@gmail.com>
2024-05-08 00:18:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer","FrontEnd Developer","BackEnd Developer", "FullStack Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay:1000,
+    loop:true
+})
